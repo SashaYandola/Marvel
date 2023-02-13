12a058b9fda42767ca5568108a46aa9c0a8119c3
Sasha Yandola <78899037+SashaYandola@users.noreply.github.com>
2023-02-13 20:22:17 +0200

    new page, component comicsList

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,7 +3,9 @@ import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
+import ComicsList from "../comicsList/ComicsList";
 import ErrorBoundary from '../errorBoundary/ErrorBoundary';
+import AppBanner from "../appBanner/AppBanner";
 
 import decoration from '../../resources/img/vision.png';
 
@@ -30,6 +32,10 @@ const App = () => {
                     </ErrorBoundary>
                 </div>
                 <img className="bg-decoration" src={decoration} alt="vision" />
+                {/* <AppBanner/>
+                <ErrorBoundary>
+                    <ComicsList/>
+                </ErrorBoundary> */}
             </main>
         </div>
     )
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,24 +1,28 @@
 import { useHttp } from "../hooks/http.hook";
 
-const useMarvelService  = () => {
+const useMarvelService = () => {
 
-    const {loading, request, error, clearError} = useHttp();
+    const { loading, request, error, clearError } = useHttp();
 
-   const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-   const _apiKey = 'apikey=6c6e097a64d28f62c652b76a6f9b8a61';
-   const _baseOffset = 300;
+    const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    const _apiKey = 'apikey=6c6e097a64d28f62c652b76a6f9b8a61';
+    const _baseOffset = 300;
 
-    const getAllCharacters = async (offset = _baseOffset) =>{
+    const getAllCharacters = async (offset = _baseOffset) => {
         const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
         return res.data.results.map(_transformCharacter);
     }
-    const getCharacter = async (id) =>{
-       const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
-       return _transformCharacter(res.data.results[0]);
+    const getCharacter = async (id) => {
+        const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
+        return _transformCharacter(res.data.results[0]);
+    }
+    const getAllComics = async (offset = _baseOffset) => {
+        const res = await request(`${_apiBase}comics?limit=8&offset=${offset}&${_apiKey}`);
+        return res.data.results.map(_transformComics);
     }
 
-    const _transformCharacter = (char) =>{
-        return{
+    const _transformCharacter = (char) => {
+        return {
             id: char.id,
             name: char.name,
             description: char.description ? `${char.description.slice(0, 210)}...` : 'DESCRIPTION NOT FOUND',
@@ -27,10 +31,26 @@ const useMarvelService  = () => {
             wiki: char.urls[1].url,
             comics: char.comics.items,
         }
-        
     }
 
-    return {loading, error, getAllCharacters, getCharacter, clearError}
-
+    const _transformComics = (comics) => {
+        return {
+            id: comics.id,
+            title: comics.title,
+            description: comics.description || "There is no description",
+            pageCount: comics.pageCount
+                ? `${comics.pageCount} p.`
+                : "No information about the number of pages",
+            thumbnail: comics.thumbnail.path + "." + comics.thumbnail.extension,
+            language: comics.textObjects[0]?.language || "en-us",
+            // optional chaining operator
+            price: comics.prices[0].price
+                ? `${comics.prices[0].price}$`
+                : "not available",
+            url: comics.urls[0].url,
+        }
+    }
+    return { loading, error, getAllCharacters, getCharacter, getAllComics, clearError };
 }
+
 export default useMarvelService;
