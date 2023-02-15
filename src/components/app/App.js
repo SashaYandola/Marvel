0a0c19e995ae756f8825448cbcaae5e709c98117
Sasha Yandola <78899037+SashaYandola@users.noreply.github.com>
2023-02-15 04:16:17 +0200

    add navigation, react-router-dom

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,45 +1,22 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ComicsList from "../comicsList/ComicsList";
-import ErrorBoundary from '../errorBoundary/ErrorBoundary';
-import AppBanner from "../appBanner/AppBanner";
-
-import decoration from '../../resources/img/vision.png';
+import { MainPage, ComicsPage } from "../pages";
 
 const App = () => {
-
-    const [selectChar, setSelectChar] = useState(null);
-
-    function onCharSelected(id) {
-        setSelectChar(id);
-    }
     return (
-        <div className="app">
-            <AppHeader />
-            <main>
-                <ErrorBoundary>
-                    <RandomChar />
-                </ErrorBoundary>
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList selectChar={onCharSelected} />
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectChar} />
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision" />
-                {/* <AppBanner/>
-                <ErrorBoundary>
-                    <ComicsList/>
-                </ErrorBoundary> */}
-            </main>
-        </div>
-    )
+        <Router>
+            <div className="app">
+                <AppHeader />
+                <main>
+                    <Routes>
+                        <Route path="/Marvel" element={<MainPage />} />
+                        <Route path="/comics" element={<ComicsPage />} />
+                    </Routes>
 
+                </main>
+            </div>
+        </Router>
+    )
 }
 
 export default App;
